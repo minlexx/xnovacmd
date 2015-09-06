b42f925c5c286f03a025f884ae40fcfe7724aafa
Alexey Min <alexey.min@gmail.com>
2015-09-06 18:28:38 +0500

    Overview Widget manually setup sub-windows to enable traslation.

--- ui/overview.py
+++ ui/overview.py
@@ -1,6 +1,6 @@
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QToolBox
 from PyQt5.QtGui import QIcon
 
 from .xnova.xn_data import fraction_from_name
@@ -22,39 +22,62 @@ class OverviewWidget(QWidget):
 
     def load_ui(self):
         self.ui = uic.loadUi(self.uifile, self)
-        self.ui.tw_accStats.setColumnWidth(0, 80)
+        self.ui.gb_account.setTitle(self.tr('Player:'))
+        self.ui.tw_accStats.setColumnWidth(0, 120)
         self.ui.tw_accStats.setColumnWidth(1, 300)
+        # init translatable table rows values in 1st column
+        self.set_as_item(0, 0, self.tr('Buildings:'))
+        self.set_as_item(1, 0, self.tr('Fleet:'))
+        self.set_as_item(2, 0, self.tr('Defense:'))
+        self.set_as_item(3, 0, self.tr('Science:'))
+        self.set_as_item(4, 0, self.tr('Total:'))
+        self.set_as_item(5, 0, self.tr('Industry:'))
+        self.set_as_item(6, 0, self.tr('Military:'))
+        self.set_as_item(7, 0, self.tr('Wins/Losses:'))
+        self.set_as_item(8, 0, self.tr('Credits:'))
+        self.set_as_item(9, 0, self.tr('Fraction:'))
+        self.set_as_item(10, 0, self.tr('Alliance:'))
+        # tool box items
+        self.ui.toolBox.setItemText(0, self.tr('Overview'))
+        self.ui.toolBox.setItemText(1, self.tr('Stats'))
+        self.ui.toolBox.setCurrentIndex(1)
+        self.ui.toolBox.currentChanged.connect(self.on_tb_currentChanged)
+
+    def set_as_item(self, row: int, col: int, text):
+        twi = QTableWidgetItem(str(text))
+        self.ui.tw_accStats.setItem(row, col, twi)
 
     def update_account_info(self):
-        def set_as_item(tw, row: int, col: int, text):
-            twi = QTableWidgetItem(str(text))
-            tw.setItem(row, col, twi)
         a = self.world.get_account_info()
         self.ui.gb_account.setTitle(self.tr('Player: {0} (id={1})').format(a.login, a.id))
-        set_as_item(self.ui.tw_accStats, 0, 1, self.tr('{0} rank {1}').format(
+        self.set_as_item(0, 1, self.tr('{0} rank {1}').format(
             a.scores.buildings, a.scores.buildings_rank))
-        set_as_item(self.ui.tw_accStats, 1, 1, self.tr('{0} rank {1}').format(
+        self.set_as_item(1, 1, self.tr('{0} rank {1}').format(
             a.scores.fleet, a.scores.fleet_rank))
-        set_as_item(self.ui.tw_accStats, 2, 1, self.tr('{0} rank {1}').format(
+        self.set_as_item(2, 1, self.tr('{0} rank {1}').format(
             a.scores.defense, a.scores.defense_rank))
-        set_as_item(self.ui.tw_accStats, 3, 1, self.tr('{0} rank {1}').format(
+        self.set_as_item(3, 1, self.tr('{0} rank {1}').format(
             a.scores.science, a.scores.science_rank))
         # total: 15156 rank 195 (+2)
         rank_delta_str = '+' + str(a.scores.rank_delta)  # explicit "+" sign
         if a.scores.rank_delta < 0:
             rank_delta_str = str(a.scores.rank_delta)
-        set_as_item(self.ui.tw_accStats, 4, 1, self.tr('{0} rank {1} ({2})').format(
+        self.set_as_item(4, 1, self.tr('{0} rank {1} ({2})').format(
             a.scores.total, a.scores.rank, rank_delta_str))
-        set_as_item(self.ui.tw_accStats, 5, 1, self.tr('{0} lv ({1}/{2} exp)').format(
+        self.set_as_item(5, 1, self.tr('{0} lv ({1}/{2} exp)').format(
             a.scores.industry_level, a.scores.industry_exp[0], a.scores.industry_exp[1]))
-        set_as_item(self.ui.tw_accStats, 6, 1, self.tr('{0} lv ({1}/{2} exp)').format(
+        self.set_as_item(6, 1, self.tr('{0} lv ({1}/{2} exp)').format(
             a.scores.military_level, a.scores.military_exp[0], a.scores.military_exp[1]))
-        set_as_item(self.ui.tw_accStats, 7, 1, self.tr('{0} W / {1} L').format(a.scores.wins, a.scores.losses))
-        set_as_item(self.ui.tw_accStats, 8, 1, a.scores.credits)
-        set_as_item(self.ui.tw_accStats, 10, 1, a.alliance_name)
+        self.set_as_item(7, 1, self.tr('{0} W / {1} L').format(a.scores.wins, a.scores.losses))
+        self.set_as_item(8, 1, a.scores.credits)
+        self.set_as_item(10, 1, a.alliance_name)
         # fraction
         fr = fraction_from_name(a.scores.fraction)
         if fr:
             icon = QIcon(':/i/{0}'.format(fr.ico_name))
             twi = QTableWidgetItem(icon, str(a.scores.fraction))
             self.ui.tw_accStats.setItem(9, 1, twi)
+
+    @pyqtSlot(int)
+    def on_tb_currentChanged(self, index):
+        logger.debug('current changed: {0}'.format(index))
