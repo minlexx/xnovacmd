fadb95a168065b72cebcb0f25540e9e2b5bcb250
Alexey Min <alexey.min@gmail.com>
2015-09-06 17:05:08 +0500

    Begin add support for multiple languages and translations

---
+++ translations/xnovacmd_ru.ts
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru" sourcelanguage="en">
+<context>
+    <name>FlightsWidget</name>
+    <message>
+        <location filename="flights_widget.ui" line="20"/>
+        <source>Form</source>
+        <translation>Форма</translation>
+    </message>
+    <message>
+        <location filename="flights_widget.ui" line="59"/>
+        <source>Fleets in space: 0</source>
+        <translation>Флоты в полете: 0</translation>
+    </message>
+</context>
+<context>
+    <name>LoginWidget</name>
+    <message>
+        <location filename="login_widget.ui" line="17"/>
+        <source>Login</source>
+        <translation>Логин</translation>
+    </message>
+    <message>
+        <location filename="login_widget.ui" line="31"/>
+        <source>Login (E-Mail):</source>
+        <translation>Логин (E-Mail):</translation>
+    </message>
+    <message>
+        <location filename="login_widget.ui" line="51"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="login_widget.ui" line="69"/>
+        <source>Remember login</source>
+        <translation>Запомнить логин</translation>
+    </message>
+    <message>
+        <location filename="login_widget.ui" line="93"/>
+        <source>Login!</source>
+        <translation>Войти!</translation>
+    </message>
+    <message>
+        <location filename="login_widget.ui" line="96"/>
+        <source>Log in with email/password</source>
+        <translation>Авторизоваться с введёнными логином и паролем</translation>
+    </message>
+    <message>
+        <location filename="login_widget.ui" line="103"/>
+        <source>Register</source>
+        <translation>Регистрация</translation>
+    </message>
+    <message>
+        <location filename="login_widget.ui" line="106"/>
+        <source>Open a web browser with registration page</source>
+        <translation>Открыть страницу регистрации в браузере</translation>
+    </message>
+    <message>
+        <location filename="login_widget.ui" line="116"/>
+        <source>loading...</source>
+        <translation>загрузка...</translation>
+    </message>
+</context>
+<context>
+    <name>MainWidget</name>
+    <message>
+        <location filename="main.ui" line="14"/>
+        <source>XNova Commander</source>
+        <translation>XNova Commander</translation>
+    </message>
+    <message>
+        <location filename="main.ui" line="96"/>
+        <source>Planets:</source>
+        <translation>Планеты:</translation>
+    </message>
+</context>
+<context>
+    <name>MyApplication</name>
+    <message>
+        <location filename="app.py" line="21"/>
+        <source>Test translator</source>
+        <translation>Проверка переводчика</translation>
+    </message>
+</context>
+</TS>
