eeb0d2a99329d2fd7d08d6e7aead8b46a6f0baf3
Alexey Min <alexey.min@gmail.com>
2015-09-07 18:25:38 +0500

    Linux script to update translations file (and release it...)

---
+++ update_translations.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+TS_FILES="translations/xnovacmd_ru.ts"
+
+SOURCES="app.py \
+    ui/main.ui \
+    ui/main.py \
+    ui/login_widget.ui \
+    ui/login_widget.py \
+    ui/overview.py \
+    ui/overview.py \
+    ui/flights_widget.ui \
+    ui/flights_widget.py \
+    ui/planets_panel.py \
+    ui/statusbar.py \
+    ui/widget_utils.py"
+
+echo "TS files: $TS_FILES"
+echo "Sources: $SOURCES"
+
+pylupdate5 $SOURCES -ts $TS_FILES -verbose
+lrelease $TS_FILES
+
+echo "You need to call lrelease $TS_FILES manually after translations"
