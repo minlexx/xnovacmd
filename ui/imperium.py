461077c7eef8ea7ca3a1b32a50eec314c42f5c4f
Alexey Min <alexey.min@gmail.com>
2015-10-16 06:32:16 +0500

    Imperium widget: center align columns content

--- ui/imperium.py
+++ ui/imperium.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, Qt
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTreeWidget, QTreeWidgetItem
 from PyQt5.QtGui import QIcon
 
@@ -33,12 +33,28 @@ class ImperiumWidget(QWidget):
         self._tree.show()
 
     def update_planets(self):
+        def additem_helper(item_texts, align_flag, twi_parent=None):
+            # align_flag = Qt::AlignLeft / Qt::AlignRight / Qt::AlignHCenter
+            twi = QTreeWidgetItem(item_texts)
+            for it_col in range(len(item_texts)):
+                if it_col > 0:
+                    # void QTreeWidgetItem::setTextAlignment(int column, int alignment)
+                    twi.setTextAlignment(it_col, align_flag)
+            if twi_parent is None:
+                self._tree.addTopLevelItem(twi)
+            else:
+                twi_parent.addChild(twi)
+            return True
+
         planets = self._world.get_planets()
         header_labels = ['-']
         for i in range(len(planets)):
             # header_labels.append(str(i))
             header_labels.append(planets[i].name)
+        header_labels.append('')  # empty last column
         self._tree.setHeaderLabels(header_labels)
+        # alignment of text in header labels
+        self._tree.header().setDefaultAlignment(Qt.AlignHCenter)
         # default column widths
         for i in range(len(planets)):
             if i < 1:
@@ -49,40 +65,40 @@ class ImperiumWidget(QWidget):
         item_strings = [self.tr('Name')]
         for pl in planets:
             item_strings.append(pl.name)
-        self._tree.addTopLevelItem(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter)
         # coords
         item_strings = [self.tr('Coords')]
         for pl in planets:
             item_strings.append('[{0}:{1}:{2}]'.format(pl.coords.galaxy, pl.coords.system, pl.coords.position))
-        self._tree.addTopLevelItem(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter)
         # fields
         item_strings = [self.tr('Fields')]
         for pl in planets:
             item_strings.append('{0}/{1}'.format(pl.fields_busy, pl.fields_total))
-        self._tree.addTopLevelItem(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter)
         #
         # resources
         res_root = QTreeWidgetItem([self.tr('Resources')])
         item_strings = [self.tr('Metal')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.res_current.met))
-        res_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, res_root)
         item_strings = [self.tr('Crystal')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.res_current.cry))
-        res_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, res_root)
         item_strings = [self.tr('Deit')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.res_current.deit))
-        res_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, res_root)
         item_strings = [self.tr('Energy')]
         for pl in planets:
             item_strings.append('{0} / {1}'.format(pl.energy.energy_left, pl.energy.energy_total))
-        res_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, res_root)
         item_strings = [self.tr('Charge')]
         for pl in planets:
             item_strings.append('{0}%'.format(pl.energy.charge_percent))
-        res_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, res_root)
         self._tree.addTopLevelItem(res_root)
         res_root.setExpanded(True)
         #
@@ -91,75 +107,75 @@ class ImperiumWidget(QWidget):
         item_strings = [self.tr('Metal factory')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.met_factory))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Crystal factory')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.cry_factory))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Deit factory')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.deit_factory))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Solar station')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.solar_station))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Nuclear station')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.nuclear_station))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Robotics factory')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.robotics_factory))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Nanites factory')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.nanites_factory))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Shipyard')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.shipyard))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Metal silo')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.met_silo))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Crystal silo')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.cry_silo))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Deit silo')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.deit_silo))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Lab')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.lab))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('TerraFormer')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.terraformer))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Alliance silo')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.alliance_silo))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Rocket silo')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.rocket_silo))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Lunar Base')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.lunar_base))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Lunar Phalanx')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.lunar_phalanx))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         item_strings = [self.tr('Gates')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.buildings.gates))
-        buildings_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, buildings_root)
         # add/expand
         self._tree.addTopLevelItem(buildings_root)
         buildings_root.setExpanded(True)
@@ -169,43 +185,43 @@ class ImperiumWidget(QWidget):
         item_strings = [self.tr('Rocket Launcher')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.ru))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Light Laser')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.ll))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Heavy Laser')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.tl))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Gauss')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.gauss))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Ion')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.ion))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Plasma')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.plasma))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Small Dome')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.small_dome))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Big Dome')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.big_dome))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Defender Missile')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.defender_rocket))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         item_strings = [self.tr('Attack Missile')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.defense.attack_rocket))
-        defense_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, defense_root)
         # add/expand
         self._tree.addTopLevelItem(defense_root)
         defense_root.setExpanded(True)
@@ -215,79 +231,79 @@ class ImperiumWidget(QWidget):
         item_strings = [self.tr('Small Transport')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.mt))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Big Transport')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.bt))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Light Fighter')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.li))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Heavy Fighter')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.ti))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Cruiser')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.crus))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Battleship')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.link))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Colonizer')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.col))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Refiner')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.rab))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Spy')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.spy))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Bomber')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.bomber))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Solar Satellite')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.ss))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Destroyer')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.unik))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Death Star')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.zs))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('BattleCruiser')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.lk))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('War Base')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.warbase))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Corvett')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.f_corvett))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Interceptor')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.f_ceptor))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Dreadnought')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.f_dread))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         item_strings = [self.tr('Corsair')]
         for pl in planets:
             item_strings.append('{0}'.format(pl.ships.f_corsair))
-        fleet_root.addChild(QTreeWidgetItem(item_strings))
+        additem_helper(item_strings, Qt.AlignHCenter, fleet_root)
         # add/expand
         self._tree.addTopLevelItem(fleet_root)
         fleet_root.setExpanded(True)
