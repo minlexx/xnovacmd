dc687fdbf4e6b58afd4bd0e22719fe16edba6433
Alexey Min <alexey.min@gmail.com>
2015-10-29 14:41:47 +0500

    xn_parser_planet_buildings: use datetime, and fix calculation of time left
    * use script block where exact number of second left specified instead of inexact date in server TZ
    * store information about end of building as datetime class object as local time

--- ui/xnova/xn_parser_planet_buildings.py
+++ ui/xnova/xn_parser_planet_buildings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
-# import datetime
+import datetime
 
 from .xn_parser import XNParserBase, safe_int, get_attribute, get_tag_classes, parse_time_left_str
 from . import xn_logger
@@ -25,21 +25,21 @@ class PlanetBuildingsParser(XNParserBase):
     def add_build_info(self,
                        position: int,
                        building: str, level: int,
-                       hours: int, minutes: int, seconds: int,
-                       remove_link: str):
+                       dt_end: datetime.datetime = None,
+                       remove_link: str = None):
         binfo = dict(
             position=position,  # sequence number in queue, starting from 1
             name=building, level=level,
-            hours=hours, minutes=minutes, seconds=seconds,
+            dt_end=dt_end,
             remove_link=remove_link
         )
         self.builds_in_progress.append(binfo)
         # logging
         time_str = ''
-        if hours + minutes + seconds > 0:
-            time_str = 'end {0:02}:{1:02}:{2:02}'.format(hours, minutes, seconds)
+        if dt_end is not None:
+            time_str = 'end {0}'.format(str(dt_end))
         rl_str = ''
-        if remove_link != '':
+        if remove_link is not None:
             rl_str = ' in queue, remove_link=[{0}]'.format(remove_link)
         logger.info(' ... {0}: [{1}] lv {2}, {3}{4}'.format(
             position, building, level, time_str, rl_str
@@ -55,7 +55,7 @@ class PlanetBuildingsParser(XNParserBase):
 
     def handle_data2(self, data: str, tag: str, attrs: list):
         super(PlanetBuildingsParser, self).handle_data2(data, tag, attrs)
-        tag_classes = get_tag_classes(attrs)
+        # tag_classes = get_tag_classes(attrs)
         # <td class="c" width="50%"> 1: Рудник металла 26 </td>
         if self.in_curbuild_table:
             if tag == 'td':
@@ -73,21 +73,28 @@ class PlanetBuildingsParser(XNParserBase):
                     # the last item in 'bs' is building level
                     self._level = safe_int(bs.pop())
                     self._building = ' '.join(bs)  # join building name back, without level
-            if tag == 'div':
-                # <div class="positive">21.10 21:41:33</div>
-                if tag_classes is not None:
-                    if 'positive' in tag_classes:
-                        hour, minute, second = parse_time_left_str(data)
-                        # td = datetime.timedelta(seconds=second, minutes=minute, hours=hour)
-                        # end_dt = datetime.datetime.now() + td
-                        # logger.debug('end time [{0}:{1}:{2}]'.format(hour, minute, second))
-                        self.add_build_info(self._position, self._building, self._level, hour, minute, second, '')
+            if tag == 'script':
+                script_type = get_attribute(attrs, 'type')
+                if (script_type is not None) and (script_type == 'text/javascript'):
+                    # <script type="text/javascript">BuildTimeout(429966, 1, 69255, 0);</script>
+                    # first argument to BuildTimeout is (possibly) seconds left
+                    # third is planet_id
+                    if data.startswith('BuildTimeout'):
+                        m = re.match(r'BuildTimeout\((\d+),', data)
+                        if m is not None:
+                            secs_left = safe_int(m.group(1))
+                            td = datetime.timedelta(seconds=secs_left)
+                            dt_now = datetime.datetime.today()
+                            dt_end = dt_now + td
+                            # logger.debug('dt_now={0}, td={1}, dt_end={2}'.format(dt_now, td, dt_end))
+                            self.add_build_info(self._position, self._building, self._level, dt_end)
             if tag == 'a':
+                # remove from queue (Architector only)
                 # <a href="?set=buildings&listid=2&cmd=remove&planet=54450">Удалить</a>
                 a_href = get_attribute(attrs, 'href')
                 if a_href is not None:
                     remove_link = a_href
-                    self.add_build_info(self._position, self._building, self._level, 0, 0, 0, remove_link)
+                    self.add_build_info(self._position, self._building, self._level, None, remove_link)
         return  # def handle_data2()
 
     def handle_endtag(self, tag: str):
