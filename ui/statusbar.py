fb3dd8b8b033f354f4fddef43b0239ba072b2868
Alexey Min <alexey.min@gmail.com>
2015-11-12 00:25:51 +0500

    Statusbar: remove unused test button

--- ui/statusbar.py
+++ ui/statusbar.py
@@ -18,16 +18,13 @@ class XNCStatusBar(QStatusBar):
         # testing only
         # self.btn_start = QPushButton(self.tr('start'), self)
         # self.btn_stop = QPushButton(self.tr('stop'), self)
-        self.btn_signal = QPushButton(self.tr('signal reload overview'), self)
         self.btn_test1 = QPushButton(self.tr('test parse'), self)
         # void	addPermanentWidget(QWidget * widget, int stretch = 0)
         # self.addPermanentWidget(self.btn_start)
         # self.addPermanentWidget(self.btn_stop)
-        self.addPermanentWidget(self.btn_signal)
         self.addPermanentWidget(self.btn_test1)
         # self.btn_start.clicked.connect(self.on_btn_start)
         # self.btn_stop.clicked.connect(self.on_btn_stop)
-        self.btn_signal.clicked.connect(self.on_btn_signal)
         self.btn_test1.clicked.connect(self.on_btn_test1)
         #
         self.show()
@@ -61,24 +58,9 @@ class XNCStatusBar(QStatusBar):
             self.world.signal_quit()
 
     @pyqtSlot()
-    def on_btn_signal(self):
-        # test exceptions in pyQt slots
-        # raise Exception("wow")
-        #
-        # e = MyThreadEvent('Signal!')  # QEvent descendant
-        # This will enqueue the event in the event loop of the thread the object is living in;
-        # therefore, the event will not be dispatched unless that thread has a running event loop.
-        # QCoreApplication.postEvent(self.world, e)
-        # it will not work, handler will be called from main (GUI) thread anyway
-        # because XNovaWorld instance object (QThread object) was created itself in main thread
-        #
-        # test signal to thread
-        self.world.signal(self.world.SIGNAL_RELOAD_PAGE, page_name='overview')
-
-    @pyqtSlot()
     def on_btn_test1(self):
         # test galaxy parser
-        # self.world.signal(self.world.SIGNAL_TEST_PARSE_GALAXY, galaxy=1, system=7)
+        self.world.signal(self.world.SIGNAL_TEST_PARSE_GALAXY, galaxy=1, system=7)
         # test planet buildings parser
         # self.world.signal(self.world.SIGNAL_TEST_PARSE_PLANET_BUILDINGS, planet_id=54450)
-        self.world.signal(self.world.SIGNAL_TEST_PARSE_PLANET_BUILDINGS, planet_id=69255)
+        #self.world.signal(self.world.SIGNAL_TEST_PARSE_PLANET_BUILDINGS, planet_id=69255)
