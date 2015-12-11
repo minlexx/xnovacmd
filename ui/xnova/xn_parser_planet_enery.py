a26066d61f52abfe9fba40ef2963db24e4d72b27
Alexey Min <alexey.min@gmail.com>
2015-12-11 20:22:17 +0500

    Add new parser to parse planet current energy status
    Thi thing is, imperium has NO info about planet total energy. This can be get from many pages except imperium:
    overview, buildings, fleet, research, resources, shipyard, defense... but not from imperium :(
    
    So this parser can be run on output of any pages mentioned above except imperium and galaxy.
    
    It is small and simple, gets only 2 values: current energy left on planet and planet total energy.

---
+++ ui/xnova/xn_parser_planet_enery.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+from .xn_parser import XNParserBase, safe_int, get_attribute
+from . import xn_logger
+
+logger = xn_logger.get(__name__, debug=True)
+
+
+__doc__ = '''
+Almost every page directly related to planet contains the display of
+planet's current resources and energy info at the top:
+- overview, buildings, researches, fleet, shipyard, defense, resources, merchant ...
+- imperium, galaxy do not have it (they do not display single planet)
+'''
+
+
+class PlanetEnergyParser(XNParserBase):
+    def __init__(self):
+        super(PlanetEnergyParser, self).__init__()
+        # public
+        self.energy_left = 0
+        self.energy_total = 0
+        # internals
+        self._in_eleft = False
+        self._in_etot = False
+        self.clear()
+
+    def clear(self):
+        self.energy_left = 0
+        self.energy_total = 0
+        # clear internals
+        self._in_eleft = False
+        self._in_etot = False
+
+    def handle_starttag(self, tag: str, attrs: list):
+        super(PlanetEnergyParser, self).handle_starttag(tag, attrs)
+        if tag == 'div':
+            div_title = get_attribute(attrs, 'title')
+            if div_title is None:
+                return
+            if div_title == 'Энергетический баланс':
+                self._in_eleft = True
+            return
+        if tag == 'span':
+            span_title = get_attribute(attrs, 'title')
+            if span_title is None:
+                return
+            if span_title == 'Выработка энергии':
+                self._in_etot = True
+            return
+
+    def handle_endtag(self, tag: str):
+        super(PlanetEnergyParser, self).handle_endtag(tag)
+        if tag == 'div':
+            if self._in_eleft:
+                self._in_eleft = False
+            return
+        if tag == 'span':
+            if self._in_etot:
+                self._in_etot = False
+            return
+
+    def handle_data2(self, data: str, tag: str, attrs: list):
+        super(PlanetEnergyParser, self).handle_data2(data, tag, attrs)
+        # if self._in_div_viewport_buildings:
+        #    logger.debug('  handle_data2(tag={0}, data={1}, attrs={2})'.format(tag, data, attrs))
+        if tag == 'span':
+            if self._in_eleft:
+                self.energy_left = safe_int(data)
+            return
+        if tag == 'font':
+            if self._in_etot:
+                self.energy_total = safe_int(data)
+            return
+
+# <div title="Энергетический баланс"><span class="positive">5</span></div>
+# <span title="Выработка энергии" class="hidden-xs"><font color="#00ff00">12.515</font></span>
