4467765016019f1e7aa7a2e675385fff10ce8c37
Alexey Min <alexey.min@gmail.com>
2015-12-21 11:58:29 +0500

    Script: start script as a parallel thread

--- scripts/test01.py
+++ scripts/test01.py
@@ -1,19 +1,28 @@
-import sys
-import os
-import logging
-import time
-
-from ui.xnova.xn_data import XNPlanet
-from ui.xnova.xn_world import XNovaWorld_instance
-from ui.xnova import xn_logger
-
-logger = xn_logger.get('test01', debug=True)
+from threading import Thread
 
 
-world = XNovaWorld_instance()
-planets = world.get_planets()
+def printer_thread():
+    import time
+    from ui.xnova.xn_data import XNPlanet
+    from ui.xnova.xn_world import XNovaWorld_instance
+    from ui.xnova import xn_logger
 
-logger.info('Print planets:')
-for planet in planets:
-    logger.info(planet.name, planet.coords.coords_str())
-logger.info('Done!')
+    logger = xn_logger.get('test01', debug=True)
+    world = XNovaWorld_instance()
+    planets = world.get_planets()
+
+    logger.info('Planets:')
+    for planet in planets:
+        logger.info(planet.name, planet.coords.coords_str())
+    logger.info('End planets.')
+
+    for i in range(5):
+        time.sleep(1)
+        print('   printer: {0}'.format(i))
+    print('End printer')
+
+
+# start script as a parallel thread
+thr = Thread(target=printer_thread, name='Printer_thread')
+thr.daemon = False
+thr.start()
